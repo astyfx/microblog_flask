ccc8e6b9e540777c4a57c32f4074e48a086b1fd0
astyfx <astyfx@gmail.com>
2013-06-17 22:04:54 +0900

    all refresh setting

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/astyfx/microblog/flask"
+set -gx VIRTUAL_ENV "/Users/astyfx/microblog_flask/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
